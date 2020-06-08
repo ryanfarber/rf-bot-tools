dee83c16b19fac26275ebd7be501e3083ffa412f
ryan farber <32393930+ryanfarber@users.noreply.github.com>
2020-06-08 05:45:53 -0700

    Create bot-message-parser.js

---
+++ bot-message-parser.js
@@ -0,0 +1,55 @@
+/*
+bot-message-parser.js
+
+a tool to output a bot command/trigger and the text of the message
+
+var text = '!bot hello there'
+var input = new BotMessageParser(text)
+console.log(input)
+
+*/
+
+function BotMessageParser(input, settings) {
+	settings = settings || {}
+	let allowtriggerinmessage = settings.allowtriggerinmessage || false
+
+	if (typeof input !== 'string') {
+		throw new Error('input must be a string!')
+	};
+	input = input.trim()
+	if (input == "" || input == undefined) {
+		this.trigger = undefined
+		this.text = undefined
+	};
+
+	if (!allowtriggerinmessage) {
+		this.info = 'trigger must be at start of message'
+		if (input.match(/^!\b.+?\b/g)) {
+			this.trigger = input.match(/^!\b.+?\b/g)[0]
+			this.text = input.replace(this.trigger, '').trim()
+			if (this.text == "") {
+				this.text = undefined
+			}
+		} else {
+			this.trigger = undefined
+		};
+	} else {
+		this.info = 'trigger allowed anywhere in message'
+		if (input.match(/!\b.+?\b/g)) {
+			this.trigger = input.match(/!\b.+?\b/g)[0]
+			this.text = input.replace(this.trigger, '').trim()
+			if (this.text == "") {
+				this.text = undefined
+			}
+		} else {
+			this.trigger = undefined
+		};
+	};
+
+	if (!this.trigger && input) {
+		this.text = input
+	};
+}
+
+module.exports = BotMessageParser
+
