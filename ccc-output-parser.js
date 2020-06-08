575d93c466344de7dc69493aa0b203e594addc2d
ryan farber <32393930+ryanfarber@users.noreply.github.com>
2020-06-08 04:58:20 -0700

    Update ccc-output-parser.js

--- ccc-output-parser.js
+++ ccc-output-parser.js
@@ -4,7 +4,9 @@ V1
 */
 
 function CCCOutputParser(str) {
+
 	str = str.split('|')
+	
 	this.task = str[0]
 	this.copyfrom = str[1]
 	this.copyto = str[2]
@@ -13,7 +15,7 @@ function CCCOutputParser(str) {
 	this.datacopied = str[5]
 	this.status = str[6]
 	this.exitcode = str[7]
-	
+
 	if (this.status == 'Success') {
 		this.message = `✅ **Copy Successful**\n**task**: ${this.task}\n**status**: ${this.status}\n**time elapsed**: ${this.time}\n**data copied**: ${this.datacopied}`;
 	} else 
