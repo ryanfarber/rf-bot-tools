881d522b61a303ee86231fa10d9fe4a43c79f31b
ryan farber <32393930+ryanfarber@users.noreply.github.com>
2020-06-10 00:55:43 -0700

    added CCC context and adjusted how the settings work

--- test.js
+++ test.js
@@ -4,6 +4,8 @@ var { ContextBuilder, BotMessageParser, WebhookParser, TextCleaner } = require('
 var message = new BotMessageParser('!bot Hi, my name is Ryan.  Who r u? k. that is cool i guess');
 console.log(message)
 
+const CCC = new ContextBuilder().CCCContext()
+
 var text = new TextCleaner(message.text)
 console.log(text.clean)
 
